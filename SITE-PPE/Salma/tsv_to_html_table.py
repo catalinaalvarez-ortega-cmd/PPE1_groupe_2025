317bcff5f948efab4d368b123e4b46d4cac947fd
salmac <salmachatoui@icloud.com>
2026-01-02 12:35:32 +0100

    suppression des scripts dans RESULTS

--- SITE-PPE/Salma/tsv_to_html_table.py
+++
@@ -1,8 +0,0 @@
-import sys
-import pandas as pd
-
-in_path = sys.argv[1]
-out_path = sys.argv[2]
-df=pd.read_csv(in_path,sep='\t', header=0)
-with open(out_path,'w') as html_file:
-    html_file.write(df.to_html(index=False))
